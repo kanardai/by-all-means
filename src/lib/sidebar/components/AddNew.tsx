fc0c8035726ed9798eb39ca57ceb427d3ff7cd09
aneta daika <kanardai@email.cz>
2023-09-21 12:46:31 +0200

    alert for inputs added

--- src/lib/sidebar/components/AddNew.tsx
+++ src/lib/sidebar/components/AddNew.tsx
@@ -2,17 +2,19 @@
 
 import { colors } from '@/styles/colorPalette';
 import TextField from '../../components/TextField';
-import { useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import { addNewSneaker } from '@/services/backendServices';
 import { SneakerInfo } from '@/types';
 import Button from '@/lib/components/button/Button';
 import { typographySize } from '@/styles/typography';
+import { formDataAlerts, inputValidation } from '@/utils/inputValidation';
 
 type AddNewSneakerProps = {
     setShowSidebar: (response: boolean) => void;
 };
 
 export default function AddNewSneaker({ setShowSidebar }: AddNewSneakerProps) {
+    const [alerts, setAlerts] = useState<typeof formDataAlerts>([]);
     const [formData, setFormData] = useState<SneakerInfo>({
         sneakerName: '',
         brand: '',
@@ -23,6 +25,17 @@ export default function AddNewSneaker({ setShowSidebar }: AddNewSneakerProps) {
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
+
+        // because 'maxLength' is used in the input tag, which works only
+        // with 'type=text', then string is converted to an integer when saved into the object
+        if (name === 'price' || name === 'size' || name === 'year') {
+            setFormData({
+                ...formData,
+                [name]: parseInt(value) || 0,
+            });
+            return;
+        }
+
         setFormData({
             ...formData,
             [name]: value,
@@ -31,7 +44,14 @@ export default function AddNewSneaker({ setShowSidebar }: AddNewSneakerProps) {
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
+
+        if (inputValidation(formData).length !== 0) {
+            setAlerts(inputValidation(formData));
+            return;
+        }
+
         setShowSidebar(!(await addNewSneaker(formData)));
+
         setFormData({
             sneakerName: '',
             brand: '',
@@ -42,57 +62,61 @@ export default function AddNewSneaker({ setShowSidebar }: AddNewSneakerProps) {
     };
 
     return (
-        <form>
-            <TextField
-                fieldName="Name"
-                inputOnChange={handleChange}
-                inputName={'sneakerName'}
-                inputValue={formData.sneakerName}
-            />
-            <div style={{ marginBottom: `${typographySize.mmLarge}` }} />
+        <>
+            <form>
+                <TextField
+                    fieldName="Name"
+                    inputOnChange={handleChange}
+                    inputName={'sneakerName'}
+                    inputValue={formData.sneakerName}
+                    alert={alerts.includes('sneakerName')}
+                />
+                <div style={{ marginBottom: `${typographySize.mmLarge}` }} />
 
-            <TextField
-                fieldName="Brand"
-                inputName={'brand'}
-                inputOnChange={handleChange}
-                inputValue={formData.brand}
-            />
-            <div style={{ marginBottom: `${typographySize.mmLarge}` }} />
+                <TextField
+                    fieldName="Brand"
+                    inputName={'brand'}
+                    inputOnChange={handleChange}
+                    inputValue={formData.brand}
+                    alert={alerts.includes('brand')}
+                />
+                <div style={{ marginBottom: `${typographySize.mmLarge}` }} />
 
-            <TextField
-                fieldName="Price"
-                inputOnChange={handleChange}
-                inputType="number"
-                inputName={'price'}
-                inputValue={formData.price}
-            />
-            <div style={{ marginBottom: `${typographySize.mmLarge}` }} />
+                <TextField
+                    fieldName="Price"
+                    inputOnChange={handleChange}
+                    inputName={'price'}
+                    inputValue={formData.price}
+                    alert={alerts.includes('price')}
+                />
+                <div style={{ marginBottom: `${typographySize.mmLarge}` }} />
 
-            <TextField
-                fieldName="Size US"
-                inputOnChange={handleChange}
-                inputType="number"
-                inputName={'size'}
-                inputValue={formData.size}
-            />
-            <div style={{ marginBottom: `${typographySize.mmLarge}` }} />
+                <TextField
+                    fieldName="Size US"
+                    inputOnChange={handleChange}
+                    inputName={'size'}
+                    inputValue={formData.size}
+                    alert={alerts.includes('size')}
+                />
+                <div style={{ marginBottom: `${typographySize.mmLarge}` }} />
 
-            <TextField
-                fieldName="Year"
-                inputOnChange={handleChange}
-                inputType="number"
-                inputName={'year'}
-                inputValue={formData.year}
-            />
-            <div style={{ marginBottom: `${typographySize.large}` }} />
+                <TextField
+                    fieldName="Year"
+                    inputOnChange={handleChange}
+                    inputName={'year'}
+                    inputValue={formData.year}
+                    alert={alerts.includes('year')}
+                />
+                <div style={{ marginBottom: `${typographySize.large}` }} />
 
-            <Button
-                color={colors.white}
-                clickHandler={handleSubmit}
-                icon="plus"
-            >
-                Add new Sneakers
-            </Button>
-        </form>
+                <Button
+                    color={colors.white}
+                    clickHandler={handleSubmit}
+                    icon="plus"
+                >
+                    Add new Sneakers
+                </Button>
+            </form>
+        </>
     );
 }
